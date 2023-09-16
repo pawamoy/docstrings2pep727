f6f13f2d6055c6d37a6d5d968c866b2e59327017
Timothée Mazzucotelli <pawamoy@pm.me>
2023-09-16 02:28:36 +0200

    feat: Add support for generators, iterators, exceptions and warnings

--- src/docstrings2pep727/transformer.py
+++ src/docstrings2pep727/transformer.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
 from itertools import chain
+from typing import TYPE_CHECKING, Sequence
 
 import libcst as cst
-from libcst import matchers
-from griffe import Docstring
 from griffe.enumerations import DocstringSectionKind
+from libcst import matchers
+
+if TYPE_CHECKING:
+    from griffe import Docstring
 
 
 def _metadata_node(name: str, *args: cst.BaseExpression) -> cst.SubscriptElement:
@@ -14,23 +17,46 @@ def _metadata_node(name: str, *args: cst.BaseExpression) -> cst.SubscriptElement
             cst.Call(
                 func=cst.Name(value=name),
                 args=[cst.Arg(arg) for arg in args],
-            )
-        )
+            ),
+        ),
     )
 
 
 def _doc_node(value: str) -> cst.SubscriptElement:
-    return _metadata_node("doc", cst.SimpleString(value=repr(value)))
+    return _metadata_node("Doc", cst.SimpleString(value=repr(value)))
 
 
 def _name_node(value: str) -> cst.SubscriptElement:
-    return _metadata_node("name", cst.SimpleString(value=repr(value)))
+    return _metadata_node("Name", cst.SimpleString(value=repr(value)))
 
 
-def _annotated(annotation: cst.BaseExpression, doc: str, name: str | None = None) -> cst.Annotation:
-    slice_elements: list[cst.SubscriptElement] = [_doc_node(doc)]
+def _raises_node(exception: str, description: str) -> cst.SubscriptElement:
+    return _metadata_node("Raises", cst.Name(exception), cst.SimpleString(value=repr(description)))
+
+
+def _warns_node(warning: str, description: str) -> cst.SubscriptElement:
+    return _metadata_node("Warns", cst.Name(warning), cst.SimpleString(value=repr(description)))
+
+
+def _annotated(
+    annotation: cst.BaseExpression,
+    *,
+    doc: str | None = None,
+    name: str | None = None,
+    raises: Sequence[tuple[type, str]] | None = None,
+    warns: Sequence[tuple[type, str]] | None = None,
+) -> cst.Annotation:
+    slice_elements: list[cst.SubscriptElement] = []
     if name:
-        slice_elements.insert(0, _name_node(name))
+        slice_elements.append(_name_node(name))
+    if doc:
+        slice_elements.append(_doc_node(doc))
+    if raises:
+        for exception, description in raises:
+            slice_elements.append(_raises_node(str(exception), description))
+    if warns:
+        for warning, description in warns:
+            slice_elements.append(_warns_node(str(warning), description))
     return cst.Annotation(
         annotation=cst.Subscript(
             value=cst.Name(value="Annotated"),
@@ -38,8 +64,19 @@ def _annotated(annotation: cst.BaseExpression, doc: str, name: str | None = None
                 cst.SubscriptElement(cst.Index(annotation)),
                 *slice_elements,
             ],
-        )
+        ),
     )
+
+
+def _update_slice(node, get_element, docstrings):
+    for index in range(len(docstrings)):
+        element = get_element(node, index)
+        item = docstrings[index]
+        node = node.with_deep_changes(
+            element.slice,
+            value=_annotated(element.slice.value, name=item[0], doc=item[1]).annotation,
+        )
+    return node
 
 
 def _matches_generator(annotation: cst.CSTNode) -> bool:
@@ -69,7 +106,9 @@ class PEP727Transformer(cst.CSTTransformer):
         self.stack.append(node.name.value)
 
     def leave_ClassDef(
-        self, original_node: cst.ClassDef, updated_node: cst.ClassDef
+        self,
+        original_node: cst.ClassDef,
+        updated_node: cst.ClassDef,
     ) -> cst.CSTNode:
         self.stack.pop()
         return updated_node
@@ -78,65 +117,141 @@ class PEP727Transformer(cst.CSTTransformer):
         self.stack.append(node.name.value)
 
     def leave_FunctionDef(
-        self, original_node: cst.FunctionDef, updated_node: cst.FunctionDef
+        self,
+        original_node: cst.FunctionDef,
+        updated_node: cst.FunctionDef,
     ) -> cst.CSTNode:
         current_path = self.current_path
         self.stack.pop()
+
         if current_path in self.docstrings:
             docstring = self.docstrings[current_path]
             param_docstrings = []
             return_docstrings = []
+            yield_docstrings = []
+            receive_docstrings = []
             exception_docstrings = []
             warning_docstrings = []
-            for section_index, section in enumerate(docstring.parsed):
+
+            for _section_index, section in enumerate(docstring.parsed):
                 if section.kind is DocstringSectionKind.parameters:
                     param_docstrings = {param.name: param.description for param in section.value}
                 elif section.kind is DocstringSectionKind.returns:
-                    return_docstrings = [{"name": retval.name, "doc": retval.description} for retval in section.value]
+                    return_docstrings = [(returned.name, returned.description) for returned in section.value]
+                elif section.kind is DocstringSectionKind.yields:
+                    yield_docstrings = [(yielded.name, yielded.description) for yielded in section.value]
+                elif section.kind is DocstringSectionKind.receives:
+                    receive_docstrings = [(received.name, received.description) for received in section.value]
                 elif section.kind is DocstringSectionKind.raises:
-                    exception_docstrings = {exc.annotation.canonical_name: exc.description for exc in section.value}
+                    exception_docstrings = [(exc.annotation.canonical_name, exc.description) for exc in section.value]
                 elif section.kind is DocstringSectionKind.warns:
-                    warning_docstrings = {warning.annotation: warning.description for warning in section.value}
+                    warning_docstrings = [(warning.annotation, warning.description) for warning in section.value]
+
             if param_docstrings:
-                for param in chain(updated_node.params.posonly_params, updated_node.params.params, updated_node.params.kwonly_params):
+                for param in chain(
+                    updated_node.params.posonly_params, updated_node.params.params, updated_node.params.kwonly_params
+                ):
                     if param.name.value in param_docstrings:
                         updated_node = updated_node.with_deep_changes(
                             param,
                             annotation=_annotated(
                                 param.annotation.annotation,
-                                param_docstrings[param.name.value],
+                                doc=param_docstrings[param.name.value],
                             ),
                         )
-            if return_docstrings:
+
+            if yield_docstrings:
+                returns = updated_node.returns
+                if _matches_generator(returns.annotation) or _matches_iterator(returns.annotation):
+                    if isinstance(returns.annotation.slice[0].slice.value, cst.Subscript):
+                        updated_node = _update_slice(
+                            updated_node,
+                            lambda node, index: node.returns.annotation.slice[0].slice.value.slice[index],
+                            yield_docstrings,
+                        )
+                    else:
+                        updated_node = updated_node.with_deep_changes(
+                            returns.annotation.slice[0].slice,
+                            value=_annotated(
+                                returns.annotation.slice[0].slice.value,
+                                name=yield_docstrings[0][0],
+                                doc=yield_docstrings[0][1],
+                            ).annotation,
+                        )
+
+            if receive_docstrings:
                 returns = updated_node.returns
                 if _matches_generator(returns.annotation):
-                    pass
-                elif _matches_iterator(returns.annotation):
-                    pass
-                elif _matches_tuple(returns.annotation):
-                    pass
-                else:
+                    if isinstance(returns.annotation.slice[1].slice.value, cst.Subscript):
+                        updated_node = _update_slice(
+                            updated_node,
+                            lambda node, index: node.returns.annotation.slice[1].slice.value.slice[index],
+                            receive_docstrings,
+                        )
+                    else:
+                        updated_node = updated_node.with_deep_changes(
+                            returns.annotation.slice[1].slice,
+                            value=_annotated(
+                                returns.annotation.slice[0].slice.value,
+                                name=receive_docstrings[0][0],
+                                doc=receive_docstrings[0][1],
+                            ).annotation,
+                        )
+
+            if return_docstrings or exception_docstrings or warning_docstrings:
+                returns = updated_node.returns
+                kwargs = {}
+                if return_docstrings:
+                    if _matches_generator(returns.annotation):
+                        if isinstance(returns.annotation.slice[2].slice.value, cst.Subscript):
+                            updated_node = _update_slice(
+                                updated_node,
+                                lambda node, index: node.returns.annotation.slice[2].slice.value.slice[index],
+                                return_docstrings,
+                            )
+                        else:
+                            updated_node = updated_node.with_deep_changes(
+                                returns.annotation.slice[2].slice,
+                                value=_annotated(
+                                    returns.annotation.slice[0].slice.value,
+                                    name=return_docstrings[0][0],
+                                    doc=return_docstrings[0][1],
+                                ).annotation,
+                            )
+                    elif _matches_tuple(returns.annotation):
+                        updated_node = _update_slice(
+                            updated_node,
+                            lambda node, index: node.returns.annotation.slice[index],
+                            return_docstrings,
+                        )
+                    else:
+                        kwargs["name"] = return_docstrings[0][0]
+                        kwargs["doc"] = return_docstrings[0][1]
+
+                if exception_docstrings or warning_docstrings or kwargs:
                     updated_node = updated_node.with_changes(
                         returns=_annotated(
                             returns.annotation,
-                            doc=return_docstrings[0]["doc"],
-                            name=return_docstrings[0]["name"],
+                            raises=exception_docstrings,
+                            warns=warning_docstrings,
+                            **kwargs,
                         ),
                     )
+
         return updated_node
-    
+
     def visit_Assign(self, node: cst.Assign) -> bool | None:
         if len(node.targets) > 1:
             return None
         self.stack.append(node.targets[0])
-    
+
     def leave_Assign(self, original_node: cst.Assign, updated_node: cst.Assign) -> cst.CSTNode:
         self.stack.pop()
         return updated_node
 
     def visit_AnnAssign(self, node: cst.AnnAssign) -> bool | None:
         self.stack.append(node.target.value)
-    
+
     def leave_AnnAssign(self, original_node: cst.AnnAssign, updated_node: cst.AnnAssign) -> cst.CSTNode:
         current_path = self.current_path
         self.stack.pop()
@@ -144,7 +259,7 @@ class PEP727Transformer(cst.CSTTransformer):
             return updated_node.with_changes(
                 annotation=_annotated(
                     updated_node.annotation.annotation,
-                    self.docstrings[current_path].parsed[0].value,
-                )
+                    doc=self.docstrings[current_path].parsed[0].value,
+                ),
             )
         return updated_node
