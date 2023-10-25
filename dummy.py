e1f72112cf8a768a3e1f46b4e3a22232d2c6c282
Timothée Mazzucotelli <pawamoy@pm.me>
2023-10-25 19:40:55 +0200

    chore: Update dummy script

--- dummy.py
+++ dummy.py
@@ -1,6 +1,8 @@
 """Module docstring."""
 
 import warnings
+from typing import Generator
+
 
 attr: int = 0
 """Module attribute docstring."""
@@ -30,3 +32,44 @@ class Class:
         if not parameter:
             raise ValueError("parameter cannot be empty")
         return parameter * 2
+
+
+def single_yields_receives_returns() -> Generator[int, str, bytes]:
+    """Summary.
+
+    Yields:
+        one: `1`.
+
+    Receives:
+        one: `"1"`.
+
+    Returns:
+        one: `b"1"`.
+    """
+
+
+def multiple_returns() -> tuple[int, int]:
+    """Summary.
+    
+    Returns:
+        one: `1`.
+        two: `2`.
+    """
+    return 1, 2
+
+
+def multiple_yields_receives_returns() -> Generator[tuple[int, int], tuple[str, str], tuple[bytes, bytes]]:
+    """Summary.
+
+    Yields:
+        one: `1`.
+        two: `2`.
+
+    Receives:
+        one: `"1"`.
+        two: `"2"`.
+
+    Returns:
+        one: `b"1"`.
+        two: `b"2"`.
+    """
